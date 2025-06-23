b74a24b2fb36e4ae1301bcbb0aa82ec18a6248db
Sitripi0 <216026207+Sitripi0@users.noreply.github.com>
2025-06-23 18:03:40 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,78 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+
+    }
+
+    getFormattedAmount() {
+        return (`${this.amount} €`)
+    }
+
+
+}
+
+const exampleAmount = new Entry("", 100, "");
+exampleAmount.getFormattedAmount();
+
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    }
+
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description);
+        this.type = "expense";
+        this.paid = paid;
+    }
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    }
+
+}
+
+const exampleExpense = new Expense("", 99, "");
+exampleExpense.getFormattedAmount();
+
+
+
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+
+        this.entries = [];
+
+    }
+    addEntry(entry) {
+        this.entries.push(entry);
+        return this.entries;
+    }
+
+    getCurrentBalance() {
+
+        let balance = 0;
+
+        if (this.entries.length === 0) {
+            return 0;
+        }
+        
+        for (let i=0; i<this.entries.length;i++){
+            let entry = this.entries[i];
+            if (entry.type === "income"){
+                balance += entry.amount;
+            }else if (entry.type==="expense"){
+                balance -=entry.amount;
+            }
+        } return balance
+    }      
+}
